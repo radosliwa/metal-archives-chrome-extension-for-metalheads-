1657dd51734394ac220f66d6efb4c2d164e1e9e3
Radosław Śliwa <radoslaw.sliwa@execon.pl>
2020-05-08 21:39:06 +0200

    refaktor

--- src/js/main/popup.js
+++ src/js/main/popup.js
@@ -1,50 +1,60 @@
-const submit = document.querySelector('#submitBtn');
-const bandInput = document.querySelector('#bandName');
-const album = document.querySelector('#album');
 
 document.addEventListener('DOMContentLoaded', ev => {
-    // collecting data-------------------------
-    let structure = collectData();
-    /*---------------------------------------*/
+    const submit = document.querySelector('#submitBtn');
+    const bandInput = document.querySelector('#bandName');
+    const album = document.querySelector('#album');
 
-    // populate storage and redirect-------------------------
-    document.addEventListener('keypress', (e) => {
-        var code = (e.keyCode ? e.keyCode : e.which);
-        if (code == 13 && Object.keys(structure).length !== 0) {
-            populateStorage(structure);
-            redirectToMA();
-            window.close();
+    const maHandler = {
+        structure: '',
+        init() {
+            // collecting data-------------------------
+            this.structure = this.collectData();
+            /*---------------------------------------*/
+            this.bindEvents();
+        },
+        collectData() {
+            let obj = {}
+            document.querySelector('.inputs').addEventListener('input', (e) => {
+                if (e.target.matches('#bandName')) {
+                    obj.category = 'band_name';
+                    obj.input = e.target.value;
+                }
+                if (e.target.matches('#album')) {
+                    obj.category = 'album_title';
+                    obj.input = e.target.value;
+                }
+            })
+            return obj;
+        },
+        bindEvents() {
+            // populate storage and redirect-------------------------
+            document.addEventListener('keypress', (e) => {
+                var code = (e.keyCode ? e.keyCode : e.which);
+                if (code == 13 && Object.keys(this.structure).length !== 0) {
+                    this.populateStorage(this.structure);
+                    this.redirectToMA();
+                    window.close();
+                }
+            })
+            submit.addEventListener('click', () => {
+                this.populateStorage(structure);
+                this.redirectToMA();
+                window.close();
+            })
+        },
+        populateStorage(obj) {
+            if (Object.keys(obj).length !== 0) {
+                chrome.storage.sync.set({ 'ma-structure': JSON.stringify(obj) }, null);
+            }
+        },
+        redirectToMA() {
+            chrome.tabs.update({
+                url: "https://www.metal-archives.com/", active: true
+            })
         }
-    })
-    submit.addEventListener('click', () => {
-        populateStorage(structure);
-        redirectToMA();
-        window.close();
-    })
-    /*---------------------------------------*/
+    }
+    maHandler.init();
+
 })
 
-function collectData(){
-    let obj = {}
-    document.querySelector('.inputs').addEventListener('input', (e) => {
-        if (e.target.matches('#bandName')) {
-            obj.category = 'band_name';
-            obj.input = e.target.value;
-        }
-        if (e.target.matches('#album')) {
-            obj.category = 'album_title';
-            obj.input = e.target.value;
-        }
-    })
-    return obj;
-}
-function populateStorage(obj) {
-    if (Object.keys(obj).length !== 0) {
-        chrome.storage.sync.set({ 'ma-structure': JSON.stringify(obj) }, null);
-    }
-}
-function redirectToMA() {
-    chrome.tabs.update({
-        url: "https://www.metal-archives.com/", active: true
-    })
-}
+
